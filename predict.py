8e8e954f496a9517f35a2da3e974e2577b7b96ee
GuangrongZhao <45409518+GuangrongZhao@users.noreply.github.com>
2024-04-18 17:24:30 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -101,13 +101,13 @@ if __name__ == '__main__':
     orders = ['1_0_1', '1_0_2', '2_0_1', '2_0_2']
     for order in orders:
         origin_data_dir = args.data_dir + '/EV_Eye_dataset/raw_data/Data_davis/user' + str(
-            user) + '/' + whicheye + '/session_' + order + '/events/'
+            user) + '/' + whicheye + '/session_' + order
 
         assert os.path.exists(origin_data_dir), " please check your data directory:" + origin_data_dir
 
         origin_paths = []
 
-        paths = glob.glob(os.path.join(origin_data_dir, 'frames/', '*.png'))
+        paths = glob.glob(os.path.join(origin_data_dir, '/frames/', '*.png'))
         origin_paths.extend(paths)
 
         # target_data_dir = args.data_dir + '/EV_Eye_dataset/processed_data/Data_davis_predict/user' + str(
